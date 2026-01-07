6518b413cefd8915a40acc7a2821083a8df5da81
Karthik K <karthiknetworklink@gmail.com>
2026-01-07 15:56:23 +0530

    fix: more

--- lib/src/sync_surface.rs
+++ lib/src/sync_surface.rs
@@ -1,29 +1,29 @@
 //! This is a module that contains a synchronous function to create a new `MySurface`.
-//! 
+//!
 //! Available with `sync` feature. (not the default one)
-//! 
+//!
 //! Can be enabled in `dependencies` in `Cargo.toml` as below.
-//! 
+//!
 //! ```toml
 //! wgpu_quick_start = { version="0.28.3", features = ["sync"] }
 //! ```
-//! 
-//! 
+//!
+//!
 use super::GPUStarterResult;
 use super::my_surface::MySurface;
 use log::info;
 use winit::window::Window;
 
 /// This synchronous function helps create a new surface from the given window.
-/// 
+///
 /// Available with `sync` feature. (Hence, not available by default).
-/// 
+///
 /// Can be enabled as below in the crate when being used.
-/// 
+///
 /// ```toml
 /// wgpu_quick_start = { version="0.28.3", features = ["sync"] }
 /// ```
-/// 
+///
 #[cfg(feature = "sync")]
 pub fn create_new_surface<'a>(window: Box<dyn Window>) -> GPUStarterResult<MySurface<'a>> {
     let size = window.surface_size();
