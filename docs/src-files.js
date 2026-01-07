1bd93005a170ee710b54d3a1fd5fdf74f5171d7f
Karthik K <karthiknetworklink@gmail.com>
2026-01-07 15:45:07 +0530

    refactor: more

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["wgpu_quick_start",["",[],["gpu_instance.rs","lib.rs","my_shader.rs","my_surface.rs"]]]]');
-//{"start":19,"fragment_lengths":[88]}
+createSrcSidebar('[["wgpu_quick_start",["",[],["gpu_instance.rs","lib.rs","my_shader.rs","my_surface.rs","sync_surface.rs"]]]]');
+//{"start":19,"fragment_lengths":[106]}
