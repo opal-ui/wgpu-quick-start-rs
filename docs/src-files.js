b5dfd0ab5c153a0fce7ee657c974f737991d4dc3
Karthik K <karthiknetworklink@gmail.com>
2026-01-08 11:24:03 +0530

    fix: more

---
+++ docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["wgpu_quick_start",["",[],["gpu_instance.rs","lib.rs","my_shader.rs","my_surface.rs","render_pass_factory.rs","sync_surface.rs"]]]]');
+//{"start":19,"fragment_lengths":[131]}
--- lib/src/sync_surface.rs
+++ lib/src/sync_surface.rs
@@ -9,6 +9,41 @@
 //! ```
 //!
 //!
+//! ## Usage
+//!
+//! Following is an example of using `create_new_surface` from a "winit" `Window`.
+//!
+//!
+//! ```rust
+//! use wgpu_quick_start::{my_surface::MySurface, sync_surface::create_new_surface};
+//! use winit::{event::WindowEvent, window::WindowAttributes};
+//! use winit_app::{app_listener::AppWindowEvent, application::Application};
+//!
+//!
+//! fn launch() -> Result<(), Box<dyn std::error::Error>> {
+//!    let winit_app = Application::new();
+//!    let mut opt_surface: Option<Box<MySurface<'_>>> = None;
+//!    winit_app.run(
+//!        WindowAttributes::default().with_title("wgpu starter app"),
+//!        move |app_window_event| match app_window_event {
+//!            AppWindowEvent::NewWindow(window) => match create_new_surface(window) {
+//!                Ok(value) => {
+//!                    opt_surface = Some(Box::new(value));
+//!                }
+//!                Err(err) => {
+//!                    // warning - Error creating new surface from the window
+//!                }
+//!            },
+//!            AppWindowEvent::OnWindowEvent(event, event_loop) => {
+//!                if let Some(local_surface) = opt_surface.as_mut() {
+//!                     // Handle those events
+//!                }
+//!            }
+//!        },
+//!    )?;
+//!    Ok(())
+//! }
+//! ```
 use super::GPUStarterResult;
 use super::my_surface::MySurface;
 use log::info;
